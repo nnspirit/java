c40f95970a46a50560c1b73b21cab1c0bf8d5152
nnspirit <54840802+nnspirit@users.noreply.github.com>
2019-09-15 19:36:31 +0800

    Add files via upload

---
+++ ComputeArea.java
@@ -0,0 +1,11 @@
+
+public class ComputeArea {
+      public static void main(String[] args) {
+    	  double radius;
+    	  double area;
+    	  radius =20;
+    	  area=radius*radius*3.14159;
+    	  System.out.printf("The area for the circle of radius "+
+    	   radius + " is "+ area);
+      }
+}
